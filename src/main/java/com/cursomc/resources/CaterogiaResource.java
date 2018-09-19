f42607b730b05a1480f16666c34df589113db6bf
Divino Silva <divinorps@gmail.com>
2018-09-19 13:07:06 -0300

    Entidade categoria criada

--- src/main/java/com/cursomc/resources/CaterogiaResource.java
+++ src/main/java/com/cursomc/resources/CaterogiaResource.java
@@ -1,15 +1,28 @@
 package com.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CaterogiaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando corretamente!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria (1, "Informática");
+		Categoria cat2 = new Categoria (2, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 }
