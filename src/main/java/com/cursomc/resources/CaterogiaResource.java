267a638a599dedf8296c705221c468954d9084d1
Divino Silva <divinorps@gmail.com>
2018-09-19 14:31:20 -0300

    Criando repository e categoria

--- src/main/java/com/cursomc/resources/CaterogiaResource.java
+++ src/main/java/com/cursomc/resources/CaterogiaResource.java
@@ -1,28 +1,28 @@
 package com.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.service.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CaterogiaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria (null, "Informática");
-		Categoria cat2 = new Categoria (null, "Escritório");
+		Categoria obj = categoriaService.buscar(id);
 		
-		List<Categoria> categorias = new ArrayList<>();
-		categorias.add(cat1);
-		categorias.add(cat2);
-		
-		return categorias;
+		return ResponseEntity.ok().body(obj);
 	}
+	
 }
---
+++ src/main/java/com/cursomc/service/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.cursomc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursomc.domain.Categoria;
+import com.cursomc.reposotiries.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> categoria = repository.findById(id);
+		return categoria.orElse(null); 
+	}
+}
