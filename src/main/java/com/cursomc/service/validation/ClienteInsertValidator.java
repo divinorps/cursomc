d7fe7f66b567624d5848f714e9fa8815ef3d1af8
Divino Silva <divinorps@gmail.com>
2018-10-09 13:27:45 -0300

    Validação personalizada: email no pode ser repetido na inclusão de Cliente

--- src/main/java/com/cursomc/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/cursomc/service/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cursomc.domain.Cliente;
 import com.cursomc.domain.enuns.TipoCliente;
 import com.cursomc.dto.ClienteNewDTO;
+import com.cursomc.reposotiries.ClienteRepository;
 import com.cursomc.resources.exception.FieldMessage;
 import com.cursomc.service.validation.utils.ValidaCPFCNPJ;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getId()) && !ValidaCPFCNPJ.isValidCNPJ(objDto.getCpfCnpj())) {
 			list.add(new FieldMessage("cpfCnpj","CNPJ inválido"));
+		}
+		
+		Cliente cliente = repository.findByEmail(objDto.getEmail());
+		if (cliente != null) {
+			list.add(new FieldMessage("email",  "Email já cadastrado."));
 		}
 		
 		for (FieldMessage e : list) {
