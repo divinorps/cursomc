5e8e0b2041fef87d18a62830c882963114e420d6
Divino Reis Pinto da Silva <divino.silva@mpdft.mp.br>
2019-10-07 14:35:27 -0300

    Inserindo pedido

--- src/main/java/com/cursomc/resources/PedidoResource.java
+++ src/main/java/com/cursomc/resources/PedidoResource.java
@@ -1,11 +1,17 @@
 package com.cursomc.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursomc.domain.Pedido;
 import com.cursomc.service.PedidoService;
@@ -25,6 +31,15 @@ public class PedidoResource {
 		Pedido obj = pedidoService.find(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido objPedido){
+		objPedido = pedidoService.insert(objPedido);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(objPedido.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/cursomc/service/PedidoService.java
+++ src/main/java/com/cursomc/service/PedidoService.java
@@ -1,23 +1,67 @@
 package com.cursomc.service;
 
+import java.util.Date;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cursomc.domain.ItemPedido;
+import com.cursomc.domain.PagamentoComBoleto;
 import com.cursomc.domain.Pedido;
+import com.cursomc.domain.enuns.EstadoPagamento;
 import com.cursomc.exception.ObjetoNaoEncontradoException;
+import com.cursomc.reposotiries.ItemPedidoRepository;
+import com.cursomc.reposotiries.PagamentoRepository;
 import com.cursomc.reposotiries.PedidoRepository;
+import com.cursomc.reposotiries.ProdutoRepository;
 
 @Service
 public class PedidoService {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	
+	@Autowired 
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired 
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired 
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired 
+	private BoletoService boletoService;
 			
 	public Pedido find(Integer id) throws ObjetoNaoEncontradoException {
 		Optional<Pedido> pedido = pedidoRepository.findById(id);
 		return pedido.orElseThrow(() -> new ObjetoNaoEncontradoException(
 				"Objeto não encontrado! Id: " +	id + ", Tipo: " + Pedido.class.getSimpleName())); 
+	}
+
+	@Transactional
+	public @Valid Pedido insert(@Valid Pedido objPedido) {
+		objPedido.setId(null);
+		objPedido.setInstante(new Date());
+		objPedido.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		objPedido.getPagamento().setPedido(objPedido);
+		if(objPedido.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto)objPedido.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, objPedido.getInstante());
+		}
+		objPedido = pedidoRepository.save(objPedido);
+		pagamentoRepository.save(objPedido.getPagamento());
+		
+		for(ItemPedido ip : objPedido.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoRepository.findById(ip.getProduto().getId()).orElse(ip.getProduto()).getPreco());
+			ip.setPedido(objPedido);
+		}
+		itemPedidoRepository.saveAll(objPedido.getItens());
+		return objPedido;
 	}
 }
